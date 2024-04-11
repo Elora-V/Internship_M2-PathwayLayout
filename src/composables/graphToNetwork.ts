bcb60b2d9420ca296a983fd54e5769e79f84e883
Elora-V <elora95.vigo@gmail.com>
2024-04-11 11:13:01 +0200

    get rank and order for viz

--- src/composables/graphToNetwork.ts
+++ src/composables/graphToNetwork.ts
@@ -1,6 +1,7 @@
 import { Network } from '@metabohub/viz-core/src/types/Network';
 import type { Node } from "@metabohub/viz-core/src/types/Node";
 import  dagre  from 'dagrejs/dist/dagre.js';
+import { type } from 'os';
 
 
 
@@ -17,11 +18,14 @@ export function changeNetworkFromDagre(graph: dagre.graphlib.Graph,network: Netw
 
         // get x (if one)
         if (Object.keys(graph["_nodes"][node]).includes('x')){
-            network.nodes[node]["x"]= graph["_nodes"][node]["x"];
+            const x=graph["_nodes"][node]["x"];
+            network.nodes[node]["x"]= x;
         }
-        // get x (if one)
+            
+        // get y (if one)
         if (Object.keys(graph["_nodes"][node]).includes('y')){
-            network.nodes[node]["y"]= graph["_nodes"][node]["y"];
+            const y=graph["_nodes"][node]["y"];
+            network.nodes[node]["y"]= y;
         }
     }
 }
@@ -33,18 +37,27 @@ export function changeNetworkFromDagre(graph: dagre.graphlib.Graph,network: Netw
  * @param {object}  object return by render method from viz (renderJSON)
  * @param {Network} Network object (value of pointer)
  */
-export function changeNetworkFromViz(json: object,network: Network){
+export function changeNetworkFromViz(json: object, network: Network)  {
+    
+    const unique_y:Array<number> =[];
 
-    for (const node in json["objects"]){
-        const nodeId=json["objects"][node]["name"];
+    for (const node in json["objects"]) {
+        const nodeId = json["objects"][node]["name"];
         // get position (if one)
-        if (Object.keys(json["objects"][node]).includes('pos')){
-            const pos= json["objects"][node]["pos"].split(',');
-            network.nodes[nodeId]["x"]= parseFloat(pos[0]);
-            network.nodes[nodeId]["y"]= parseFloat(pos[1]);
+        if (Object.keys(json["objects"][node]).includes('pos')) {
+            const pos = json["objects"][node]["pos"].split(',');
+            const x = parseFloat(pos[0]);
+            const y = parseFloat(pos[1]);
+            network.nodes[nodeId]["x"] = x;
+            network.nodes[nodeId]["y"] = y;
+            if( !unique_y.includes(y)){
+                unique_y.push(y);
+            }
         }
-
     }
+    
+    assignRankOrder(network,unique_y);
+    
 }
 
 
@@ -116,4 +129,46 @@ function getNodeFromNetwork(id: string, network:Network): Node{
 
     return network["nodes"][id]; 
 
+}
+
+
+
+
+/**
+ * Take network and all the unique coordinate y, add the rank (y position like first, second..., not coordinate) and order ( first, second,....for x axis) to metadata of network.
+ * @param {Network} Network object
+ * @param unique_y array of all unique y for node position
+ */
+function assignRankOrder(network: Network, unique_y: Array<number>) {
+
+    // sort the y to know the associated rank for a y coordinate
+    unique_y.sort((a:number, b:number) => a - b);
+
+    // get the rank for each node
+    const xNodeByRank: number[][] = Array.from({ length: unique_y.length }, () => []);
+    for (const node in network["nodes"]){
+        const rank = unique_y.indexOf(network.nodes[node].y);
+        if (!Object.keys(network.nodes).includes("metadata")){
+            network.nodes[node]["metadata"]={};
+        }
+        network.nodes[node].metadata["rank"]=rank;
+        xNodeByRank[rank].push(network.nodes[node].x);
+    }
+
+    // sort the y by rank
+    xNodeByRank.forEach(sublist => {
+        sublist.sort((a, b) => a - b); 
+    });
+
+    // get the order for each node 
+    for (const node in network["nodes"]){
+        const rank= network.nodes[node].metadata["rank"];
+        if (typeof rank === 'number'){
+            const order = xNodeByRank[rank].indexOf(network.nodes[node].x);
+            network.nodes[node].metadata["order"]=order;
+        } else {
+            console.error("Rank isn't a number");
+            network.nodes[node].metadata["order"]=0; // order by default
+        }
+    }
 }
