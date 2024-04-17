cfab7559165b5d564acec51e597ccc14aa254778
Elora-V <elora95.vigo@gmail.com>
2024-04-17 10:09:48 +0200

    add viz cluster

--- src/types/VizType.ts
+++ src/types/VizType.ts
@@ -1,10 +1,20 @@
-// Graph already accessible, but the other interface can't be exported so there are duplicated here
+// Graph already accessible (see the format below, but implemented in library), but the other interface can't be exported so there are duplicated here
+// export interface Graph {
+//     name?: string
+//     strict?: boolean
+//     directed?: boolean
+//     graphAttributes?: Attributes
+//     nodeAttributes?: Attributes
+//     edgeAttributes?: Attributes
+//     nodes?: Node[]
+//     edges?: Edge[]
+//     subgraphs?: Subgraph[]
+//   }
 
 interface AttributesViz {
     [name: string]: string | number | boolean 
   }
   
-
 interface NodeViz {
 name: string
 attributes?: AttributesViz
@@ -16,12 +26,13 @@ head: string
 attributes?: AttributesViz
 }
 
-interface Subgraph {
+interface SubgraphViz {
 name?: string
+classes?:Array<string>
 graphAttributes?: AttributesViz
 nodeAttributes?: AttributesViz
 edgeAttributes?: AttributesViz
 nodes?: NodeViz[]
 edges?: EdgeViz[]
-subgraphs?: Subgraph[]
+subgraphs?: SubgraphViz[]
 }
