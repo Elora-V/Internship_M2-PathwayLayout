2e24b23a6366e20cf28f36dc590ecf0d30e483b8
marion.liotier <marion.liotier@inrae.fr>
2024-04-23 14:51:41 +0200

    refactoring

--- src/composables/__tests__/countIntersection.spec.ts
+++ src/composables/__tests__/countIntersection.spec.ts
@@ -1,17 +1,17 @@
 import { describe, expect, test } from "vitest";
 import { countIntersection } from "../countIntersections";
-import { importNetworkFromURL } from "../importNetwork";
 import { Network } from "@metabohub/viz-core/src/types/Network";
-import { GraphStyleProperties } from "@metabohub/viz-core/src/types/GraphStyleProperties";
-import { ref } from "vue";
 import * as fs from 'fs';
 import { readJsonGraph } from "../readJson";
+
+function getNetworkFromUrl(url: string): Network {
+    return (readJsonGraph(fs.readFileSync(url, { encoding: 'utf8', flag: 'r' }))).network;
+}
 
 describe("countIntersections.ts", () => {
     test("should return 1 when given a network with one crossing", async () => {
         // DATA
-        const data = readJsonGraph(fs.readFileSync('public/test_intersect/network_one_crossing.json', { encoding: 'utf8', flag: 'r' }));
-        const network: Network = data.network;
+        const network: Network = getNetworkFromUrl('public/test_intersect/network_one_crossing.json');
         
         // TEST
         const result = countIntersection(network);
