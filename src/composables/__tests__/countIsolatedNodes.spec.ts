0b0126327ec230c3ae7d1bc1ceafc2f8e6a6cd26
marion.liotier <marion.liotier@inrae.fr>
2024-04-25 10:27:56 +0200

    test for 1 isolated node

--- src/composables/__tests__/countIsolatedNodes.spec.ts
+++ src/composables/__tests__/countIsolatedNodes.spec.ts
@@ -36,4 +36,41 @@ describe("countIsolatedNodes.ts", () => {
         // EXPECT
         expect(result).toEqual(0);
     });
+    test("should return 1 when there is an isolated node", () => {
+        // DATA
+        const nodes: { [key: string]: Node } = {
+            "a": {
+                id: "a",
+                x: 0,
+                y: 0
+            },
+            "b": {
+                id: "b",
+                x: 0,
+                y: 0
+            },
+            "c": {
+                id: "c",
+                x: 0,
+                y: 0
+            }
+        };
+        const network: Network = {
+            id: "test",
+            nodes: nodes,
+            links: [
+                {   
+                    id: "a_b",
+                    source: nodes.a,
+                    target: nodes.b
+                }
+            ]
+        };
+
+        // FUNCTION
+        const result = countIsolatedNodes(network);
+
+        // EXPECT
+        expect(result).toEqual(1);
+    });
 });
