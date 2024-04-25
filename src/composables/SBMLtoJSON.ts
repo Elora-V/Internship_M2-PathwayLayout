964013df88f5dd0670bd1c50e0da8c5aa2d6a6b1
marion.liotier <marion.liotier@inrae.fr>
2024-04-25 16:23:56 +0200

    add tests

--- src/composables/SBMLtoJSON.ts
+++ src/composables/SBMLtoJSON.ts
@@ -49,148 +49,137 @@ function getRandomInt(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export async function sbml2json(sbmlFilePath: string): Promise<JsonGraphFormat> {
-    try {
-        // read xml file
-        const response = await fetch(sbmlFilePath);
-        // DATA
-        const sbmlString: string = await response.text();
-
-        return new Promise((resolve, reject) => {
-            parseString(sbmlString, { explicitArray: false }, (err, result) => {
-                if (err) {
-                    console.log("Error during the parsing of the file");
-                    console.log(err);
-                    reject(err);
-                } else {
-                    const model = result.sbml.model;
-                    // graph to return
-                    const graph: JsonGraphFormat = {
-                        graph: {
-                            id: model.$.id,
-                            type: 'metabolic',
-                            metadata: {
-                                style: {
-                                    nodeStyles: {
-                                        metabolite: {
-                                            width: 25,
-                                            height: 25,
-                                            strokeWidth: 1,
-                                            shape: 'circle'
-                                        },
-                                        reaction: {
-                                            width: 15,
-                                            height: 15,
-                                            strokeWidth: 0.5,
-                                            shape: 'rect',
-                                            fill: 'grey'
-                                        },
-                                        reversible: {
-                                            fill: 'green',
-                                            shape: 'inverseTriangle'
-                                        },
-                                        reversibleVersion: {
-                                            fill: 'red',
-                                            shape: 'triangle'
-                                        }
+export async function sbml2json(sbmlString: string): Promise<JsonGraphFormat> {
+    return new Promise((resolve, reject) => {
+        parseString(sbmlString, { explicitArray: false }, (err, result) => {
+            if (err) {
+                console.log("Error during the parsing of the file");
+                console.log(err);
+                reject(err);
+            } else {
+                const model = result.sbml.model;
+                // graph to return
+                const graph: JsonGraphFormat = {
+                    graph: {
+                        id: model.$.id,
+                        type: 'metabolic',
+                        metadata: {
+                            style: {
+                                nodeStyles: {
+                                    metabolite: {
+                                        width: 25,
+                                        height: 25,
+                                        strokeWidth: 1,
+                                        shape: 'circle'
+                                    },
+                                    reaction: {
+                                        width: 15,
+                                        height: 15,
+                                        strokeWidth: 0.5,
+                                        shape: 'rect',
+                                        fill: 'grey'
+                                    },
+                                    reversible: {
+                                        fill: 'green',
+                                        shape: 'inverseTriangle'
+                                    },
+                                    reversibleVersion: {
+                                        fill: 'red',
+                                        shape: 'triangle'
                                     }
                                 }
-                            },
-                            nodes: {},
-                            edges: []
-                        }
+                            }
+                        },
+                        nodes: {},
+                        edges: []
+                    }
+                };
+
+                // Transform species to nodes
+                const speciesList = model.listOfSpecies.species;
+                // add a metabolite node for each species
+                speciesList.forEach((species: { $: { compartment: string; id: string; name: string; species: string } }) => {
+                    graph.graph.nodes[species.$.id] = {
+                        id: species.$.id,
+                        metadata: {
+                            classes: ['metabolite'],
+                            position: {
+                                x: getRandomInt(0, 100),
+                                y: getRandomInt(0, 100)
+                            }
+                        },
+                        label: species.$.name
+                    };
+                });
+
+                // Transform reactions to nodes and edges
+                const reactions = model.listOfReactions.reaction;
+                reactions.forEach((reaction: any) => {
+                    const reactionId = reaction.$.id;
+
+                    // get the reactants and products for every reaction
+                    const reactants: any[] = [];
+                    if (reaction.listOfReactants.speciesReference != undefined && reaction.listOfReactants.speciesReference.length != undefined) {
+                        // type : array
+                        reaction.listOfReactants.speciesReference.forEach((ref: any) => {
+                            reactants.push(ref.$.species);
+                        });
+                    } else if (reaction.listOfReactants.speciesReference != undefined) {
+                        // type : object
+                        reactants.push(reaction.listOfReactants.speciesReference.$.species);
+                    }
+                    const products: any[] = [];
+                    if (reaction.listOfProducts.speciesReference != undefined && reaction.listOfProducts.speciesReference.length != undefined) {
+                        // type : array
+                        reaction.listOfProducts.speciesReference.forEach((ref: any) => {
+                            products.push(ref.$.species);
+                        });
+                    } else if (reaction.listOfProducts.speciesReference != undefined) {
+                        // type : object
+                        products.push(reaction.listOfProducts.speciesReference.$.species);
+                    }
+
+                    // add the reaction as a node
+                    graph.graph.nodes[reactionId] = {
+                        id: reactionId,
+                        metadata: {
+                            classes: ['reaction'],
+                            position: {
+                                x: getRandomInt(0, 100),
+                                y: getRandomInt(0, 100)
+                            }
+                        },
+                        label: reaction.$.name
                     };
 
-                    // Transform species to nodes
-                    const speciesList = model.listOfSpecies.species;
-                    // add a metabolite node for each species
-                    speciesList.forEach((species: { $: { compartment: string; id: string; name: string; species: string } }) => {
-                        graph.graph.nodes[species.$.id] = {
-                            id: species.$.id,
+                    // add the edges for the reaction and its reactants and products
+                    reactants.forEach((reactant: string) => {
+                        console.log(`         ${reactant}--${reactionId}`);
+                        graph.graph.edges.push({
+                            id: `${reactant}--${reactionId}`,
+                            source: reactant,
+                            target: reactionId,
                             metadata: {
-                                classes: ['metabolite'],
-                                position: {
-                                    x: getRandomInt(0, 100),
-                                    y: getRandomInt(0, 100)
-                                }
-                            },
-                            label: species.$.name
-                        };
-                    });
-
-                    // Transform reactions to nodes and edges
-                    const reactions = model.listOfReactions.reaction;
-                    reactions.forEach((reaction: any) => {
-                        const reactionId = reaction.$.id;
-
-                        // get the reactants and products for every reaction
-                        const reactants: any[] = [];
-                        if (reaction.listOfReactants.speciesReference != undefined && reaction.listOfReactants.speciesReference.length != undefined) {
-                            // type : array
-                            reaction.listOfReactants.speciesReference.forEach((ref: any) => {
-                                reactants.push(ref.$.species);
-                            });
-                        } else if (reaction.listOfReactants.speciesReference != undefined) {
-                            // type : object
-                            reactants.push(reaction.listOfReactants.speciesReference.$.species);
-                        }
-                        const products: any[] = [];
-                        if (reaction.listOfProducts.speciesReference != undefined && reaction.listOfProducts.speciesReference.length != undefined) {
-                            // type : array
-                            reaction.listOfProducts.speciesReference.forEach((ref: any) => {
-                                products.push(ref.$.species);
-                            });
-                        } else if (reaction.listOfProducts.speciesReference != undefined) {
-                            // type : object
-                            products.push(reaction.listOfProducts.speciesReference.$.species);
-                        }
-
-                        // add the reaction as a node
-                        graph.graph.nodes[reactionId] = {
-                            id: reactionId,
-                            metadata: {
-                                classes: ['reaction'],
-                                position: {
-                                    x: getRandomInt(0, 100),
-                                    y: getRandomInt(0, 100)
-                                }
-                            },
-                            label: reaction.$.name
-                        };
-
-                        // add the edges for the reaction and its reactants and products
-                        reactants.forEach((reactant: string) => {
-                            console.log(`         ${reactant}--${reactionId}`);
-                            graph.graph.edges.push({
-                                id: `${reactant}--${reactionId}`,
-                                source: reactant,
-                                target: reactionId,
-                                metadata: {
-                                    classes: ['irreversible']
-                                }
-                            });
-                        });
-                        products.forEach((product: string) => {
-                            console.log(`         ${reactionId}--${product}`);
-                            graph.graph.edges.push({
-                                id: `${reactionId}--${product}`,
-                                source: reactionId,
-                                target: product,
-                                metadata: {
-                                    classes: ['irreversible']
-                                }
-                            });
+                                classes: ['irreversible']
+                            }
                         });
                     });
+                    products.forEach((product: string) => {
+                        console.log(`         ${reactionId}--${product}`);
+                        graph.graph.edges.push({
+                            id: `${reactionId}--${product}`,
+                            source: reactionId,
+                            target: product,
+                            metadata: {
+                                classes: ['irreversible']
+                            }
+                        });
+                    });
+                });
 
-                    // return the graph object
-                    resolve(graph);
-                }
-            });
+                // return the graph object
+                resolve(graph);
+            }
         });
-    } catch (error) {
-        console.log('could not fetch the xml file !');
-        console.log(error);
-        return null;
-    }
+    });
 }
---
+++ src/composables/__tests__/SBMLtoJSON.spec.ts
@@ -0,0 +1,121 @@
+import { expect, describe, test } from 'vitest';
+import { JsonGraphFormat, sbml2json } from '../SBMLtoJSON';
+
+describe('sbml2json.ts', () => {
+    test('should return a JSON with 3 nodes', async () => {
+        fetch('2_reac_1_metab.xml')
+            .then(async (res) => {
+                // DATA
+                const sbmlString: string = await res.text();
+
+                // FUNCTION
+                const result: JsonGraphFormat = await sbml2json(sbmlString);
+                const nodes = result.graph.nodes;
+
+                // EXPECT
+                expect(nodes).toEqual(3);
+            })
+            .catch(() => {
+                console.log('could not fetch the test data !');
+                return false;
+            });
+    });
+    test('the nodes should have a position', async () => {
+        fetch('2_reac_1_metab.xml')
+            .then(async (res) => {
+                // DATA
+                const sbmlString: string = await res.text();
+
+                // FUNCTION
+                const result: JsonGraphFormat = await sbml2json(sbmlString);
+                const nodes = result.graph.nodes;
+
+                // EXPECT
+                Object.values(nodes).forEach((node) => {
+                    expect(node.metadata.position!.x).toBeDefined();
+                    expect(node.metadata.position!.y).toBeDefined();
+                });
+            })
+            .catch(() => {
+                console.log('could not fetch the test data !');
+                return false;
+            });
+    });
+    test('there should 2 nodes for the reactions', async () => {
+        fetch('2_reac_1_metab.xml')
+            .then(async (res) => {
+                // DATA
+                const sbmlString: string = await res.text();
+
+                // FUNCTION
+                const result: JsonGraphFormat = await sbml2json(sbmlString);
+                const nodes = result.graph.nodes;
+
+                // EXPECT
+                expect(nodes['erty']).toBeDefined();
+                expect(nodes['erty'].metadata.classes).toContain('reaction');
+                expect(nodes['r1']).toBeDefined();
+                expect(nodes['r1'].metadata.classes).toContain('reaction');
+            })
+            .catch(() => {
+                console.log('could not fetch the test data !');
+                return false;
+            });
+    });
+    test('there should 1 node for the metabolite', async () => {
+        fetch('2_reac_1_metab.xml')
+            .then(async (res) => {
+                // DATA
+                const sbmlString: string = await res.text();
+
+                // FUNCTION
+                const result: JsonGraphFormat = await sbml2json(sbmlString);
+                const nodes = result.graph.nodes;
+
+                // EXPECT
+                expect(nodes['m1']).toBeDefined();
+                expect(nodes['m1'].metadata.classes).toContain('metabolite');
+            })
+            .catch(() => {
+                console.log('could not fetch the test data !');
+                return false;
+            });
+    });
+    test('should return a JSON with 1 edge', async () => {
+        fetch('2_reac_1_metab.xml')
+            .then(async (res) => {
+                // DATA
+                const sbmlString: string = await res.text();
+
+                // FUNCTION
+                const result: JsonGraphFormat = await sbml2json(sbmlString);
+                const edges = result.graph.edges;
+
+                // EXPECT
+                expect(edges.length).toEqual(1);
+            })
+            .catch(() => {
+                console.log('could not fetch the test data !');
+                return false;
+            });
+    });
+    test('the edge should be between "m1" and "r1"', async () => {
+        fetch('2_reac_1_metab.xml')
+            .then(async (res) => {
+                // DATA
+                const sbmlString: string = await res.text();
+
+                // FUNCTION
+                const result: JsonGraphFormat = await sbml2json(sbmlString);
+                const edge = result.graph.edges[0];
+
+                // EXPECT
+                expect(edge.source).toEqual('m1');
+                expect(edge.target).toEqual('r1');
+            })
+            .catch(() => {
+                console.log('could not fetch the test data !');
+                return false;
+            });
+    });
+});
