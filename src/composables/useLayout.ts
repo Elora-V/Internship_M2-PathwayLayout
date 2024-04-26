51be38ae0e7777d144f85da3c3b6243d8a2f6241
Elora-V <elora95.vigo@gmail.com>
2024-04-26 14:54:48 +0200

    useLayout async

--- src/composables/useLayout.ts
+++ src/composables/useLayout.ts
@@ -35,7 +35,7 @@ export function dagreLayout(network: Network,graphAttributes={},callbackFunction
  * @param assignRank indicates if rank and order need to be infered after layout is applied
  * @param [callbackFunction=() => {}] function to do after the layout is done
  */
-export function vizLayout(network: Network,clusters:{[key:string]:Cluster}={}, graphAttributes={},assignRank:boolean=false, callbackFunction = () => {}):void {
+export async function vizLayout(network: Network,clusters:{[key:string]:Cluster}={}, graphAttributes={},assignRank:boolean=false, callbackFunction = () => {}): Promise<void> {
     console.log('Viz');
     setTimeout(async function() {
         instance().then(viz => {
