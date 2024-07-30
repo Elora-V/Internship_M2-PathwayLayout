0a0afa0ec7d610be929208a20b1ca36609dc2d45
Elora-V <elora95.vigo@gmail.com>
2024-07-30 08:52:44 +0200

    mini loop for number node

--- src/composables/applyMetrics.ts
+++ src/composables/applyMetrics.ts
@@ -7,14 +7,16 @@ import { GraphStyleProperties } from "@metabohub/viz-core/src/types/GraphStylePr
 export async function analyseAllJSON(pathListJSON: string): Promise<void> {
     const jsonFileString = await getContentFromURL(pathListJSON);
     const allJson = jsonFileString.split('\n');
+    const resultAllJSON: number[][] = [];
 
     for (const json of allJson) {
-        await analyseJSON(json);
+        const resultJSON= await analyseJSON(json);
+        resultAllJSON.push(resultJSON);
     }
-   
+   console.log(resultAllJSON);
 }
 
-async function analyseJSON(json: string): Promise<void> {
+async function analyseJSON(json: string): Promise<number[]> {
 
     // initialize network and networkStyle
     const networkForJSON = ref<Network>({ id: '', nodes: {}, links: [] });
@@ -22,14 +24,18 @@ async function analyseJSON(json: string): Promise<void> {
         nodeStyles: {},
         linkStyles: {}
     });
+    const resultAnalysis:number[]=[];
 
     // import network from JSON, and process it
     try {
         importNetworkFromURL(json, networkForJSON, networkStyleforJSON, () => {
-        console.log("\n\n\n---------------------------------------\n"+json);
-        console.log(networkForJSON.value);
+        console.log("\n\n---------------------------------------\n"+json);
+        resultAnalysis.push(Object.keys(networkForJSON.value.nodes).length);
+        resultAnalysis.push(networkForJSON.value.links.length);
     });
     } catch (error) {
         console.error("error file : " + json + "\n"+ error);
     }
+
+    return resultAnalysis;
 }
